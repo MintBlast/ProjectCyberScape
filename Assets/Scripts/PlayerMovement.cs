55bdcd52aeed84d4aca7fee591bfa6c47e93502d
MintBlast <37569924+MintBlast@users.noreply.github.com>
2020-08-18 15:33:04 +1200

    Another Scene added
    Scene added for FPS testing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,22 +6,26 @@ public class PlayerMovement : MonoBehaviour
 {
     //character controller
     public CharacterController controller;
+
     //camera movement
     public Transform cam;
     //speed of the movement - walk
     public float speed = 6f;
     //speed of the movement - run
     public float runSpeed = 12f;
-    //public Vector3 jump;
-    //public float JumpForce = 2.0f;
+    //rigidbody
+    Rigidbody playerRb;
+    
+    //jump as vector3
+    public Vector3 jump;
+    //jumpforce
+    public float JumpForce = 2.0f;
+    
     //when player is on the ground - Error: player can jump in mid-air
     public bool isGrounded;
-    //rigibody
-    private Rigidbody playerRb;
-    //animator
-    private Animator playerAnim;
-    //gravity modifier - changes gravity value
-    public float gravityModifier;
+    
+    
+
     //smooth turn time
     public float turnSmoothTime = 0.1f;
     //velocity of smooth turn
@@ -30,10 +34,9 @@ public class PlayerMovement : MonoBehaviour
     
     void Start()
     {
-        //player is the rigidbody
         playerRb = GetComponent<Rigidbody>();
-        playerAnim = GetComponent<Animator>();
-        Physics.gravity *= gravityModifier;
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        
     }
 
     // Update is called once per frame
@@ -46,20 +49,14 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         //moves in x, z axis and moves in a normal speed when moved diagonally
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-        
+
+        //creates a tiny sphere below the character's base, if the groundMask is collided with the ground, then isGrounded is true
+        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(jump * JumpForce, ForceMode.Impulse);
             isGrounded = false;
-        }
-
-        if(Input.GetKeyDown(KeyCode.W) && isGrounded)
-        {
-            playerAnim.SetBool("isWalking", true);
-        }
-        else
-        {
-            playerAnim.SetBool("isWalking", false);
         }
 
         //camera rotation

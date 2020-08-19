511f52d6dfcd502c8835049c7691bc88264ed918
MintBlast <37569924+MintBlast@users.noreply.github.com>
2020-08-19 17:04:52 +1200

    First Person Player Movement added
    Script added. Adding movement not yet finished

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -8,10 +8,13 @@ public class MouseLook : MonoBehaviour
 
     public Transform playerBody;
 
+    float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hide and lock the cursor at the center of the screen
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -19,9 +22,13 @@ public class MouseLook : MonoBehaviour
     {
         //camera moves at an X axis
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        //camera moves at an Y axis
+        //camera moves at a Y axis
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
     }
 }

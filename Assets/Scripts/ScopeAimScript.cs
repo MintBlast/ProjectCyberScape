b2d905e42ca184b5bfdb2591fa54cd83e25b4330
Mintblast <37569924+MintBlast@users.noreply.github.com>
2020-10-07 14:34:27 +1300

    Scope Improvisation
    The scope uses the dual render scope which uses two cameras: one for the player view and the other as the scope FOV which is rendered using a render texture

---
+++ Assets/Scripts/ScopeAimScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScopeAimScript : MonoBehaviour
+{
+    //animator
+	public Animator animator;
+
+	//scope overlay image
+	//public GameObject scopeOverlay;
+
+	//weapon cam
+	public GameObject weaponCamera;
+
+    //isScoped
+	private bool isScoped = false;
+
+	public Camera mainCamera;
+
+	public float scopedFOV = 5f;
+	float normalFOV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+		{
+			isScoped = !isScoped;
+			animator.SetBool("Scoped", isScoped);
+
+		}
+    }
+
+    
+}
